9d55e9347bb953f9e71b26347086d30cb5bebe79
dt_4473643431 <w1176317790@163.com>
2023-10-21 10:14:05 +0800

    优化初始化项目

---
+++ craco.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { name } from './package.json';
+
+const pathResolve = (pathUrl: string) => path.join(__dirname, pathUrl);
+
+module.exports = {
+    reactScriptsVersion: 'react-scripts' /* (default value) */,
+    webpack: {
+        alias: {
+            '@': pathResolve('src'),
+            '@assets': pathResolve('src/assets'),
+            '@components': pathResolve('src/components'),
+            '@utils': pathResolve('src/utils'),
+        },
+        configure(webpackConfig:any) {
+            // 配置扩展扩展名
+            webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
+            // 接入微前端框架qiankun的配置,不接入微前端可以不需要
+            // webpackConfig.output.library = `${name}-[name]`;
+            // webpackConfig.output.libraryTarget = 'umd';
+            // webpackConfig.output.globalObject = 'window';
+            return webpackConfig;
+        },
+    },
+    devServer: {
+        // 本地服务的端口号
+        port: 3001,
+        // 本地服务的响应头设置
+        headers: {
+            // 允许跨域
+            'Access-Control-Allow-Origin': '*',
+        },
+    },
+};
+
