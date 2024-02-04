ba4b6e665eee165da23ef9bc58620df2ada1d6f3
A2001083_王煦澄 <w1176317790@163.com>
2024-02-04 15:02:30 +0800

    feat 删除 redux 库

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import { useAppDispatch, useAppReducer } from './store';
 import { Route, Routes } from 'react-router-dom';
 import Login from './views/Login/Index';
 
 function App() {
-  const counterStore = useAppReducer((store: any) => store.counters);
-  const dispatch = useAppDispatch();
   return (
     <>
       <Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
 import { ConfigProvider } from 'antd';
 import theme from './theme';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <ConfigProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConfigProvider>
-  </Provider>
+  <ConfigProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ConfigProvider>
 );
